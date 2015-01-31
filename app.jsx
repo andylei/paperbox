5700e14e35dcb239f882dd3476dc46a5d55c4359
Andy Lei <andylei@gmail.com>
2015-01-31 00:49:38 -0500

    added fork me button

--- app.jsx
+++ app.jsx
@@ -24,6 +24,12 @@ Window = React.createClass({
         <h3>
           When you print, make sure you print at 100% size.
         </h3>
+        <a href="https://github.com/andylei/paperbox">
+          <img style={{position: 'absolute', top: 0, right: 0, border: 0}}
+               src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+               alt="Fork me on Github"
+          />
+        </a>
       </div>
     );
   }
